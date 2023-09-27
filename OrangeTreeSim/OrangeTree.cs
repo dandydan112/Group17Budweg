3fef2f21056c1e334f82179ade3a38a84524dd54
Frederik Skjoeth <ftns58402@edu.ucl.dk>
2023-09-27 09:14:06 +0200

    hej

---
+++ OrangeTreeSim/OrangeTree.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrangeTreeSim
+{
+    public class OrangeTree
+    {
+        private int age;
+        private int height;
+        private bool treeAlive;
+        private int numOranges;
+        private int orangesEaten;
+        private int eatOrange;
+        public int count;
+        public void SetAge(int Age)
+        {
+            this.age = Age;
+        }
+        public int GetAge()
+        {
+            return age;
+        }
+        public void SetHeight(int Height)
+        {
+            this.height = Height;
+        }
+        public int GetHeight()
+        {
+            return this.height;
+        }
+        public void SetTreeAlive(bool treeAlive)
+        {
+            this.treeAlive = treeAlive;
+        }
+        public bool GetTreeAlive()
+        {
+            return treeAlive;
+        }
+        public int GetOrangesEaten()
+        {
+
+            return count;
+        }
+        public int GetNumOranges()
+        {
+            return numOranges - count;
+        }
+        public void EatOrange(int eatOrange)
+        {
+            this.eatOrange = eatOrange;
+            count += eatOrange;
+        }
+        public void OneYearPasses()
+        {
+            if (treeAlive == true)
+            {
+                count = 0;
+                age++;
+                if (age < 80) { height += 2; }
+                if (age > 1 && age < 80) 
+                { 
+                    numOranges=(age-1)*5; 
+                }
+                if (age >=80) { treeAlive = false; }
+                
+            }
+            else if (treeAlive == false)
+            {
+                numOranges = 0;
+                age++;
+                count = 0;
+            }
+            
+        }
+        
+        
+    }
+
+}
---
+++ OrangeTreeSim/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace OrangeTreeSim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+
+
+        }
+    }
+}
